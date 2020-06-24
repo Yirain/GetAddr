07198815778ad68630c0675952fa96842e33330e
Nakululu Satuva <syr.chloe@gmail.com>
2020-06-24 14:55:01 +0800

    Update

---
+++ get_addr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ip_version=$1
+interfaces=${*:2}
+
+if [ -z $ip_version ]; then
+    >&2 echo "ERROR, No parameters are given!"
+    exit 1
+elif [ $ip_version == "4" ]; then
+    for interface in $interfaces;
+    do
+        var=$(ip address show $interface 2>/dev/null | grep -o -P '(?<=inet ).*(?=/)')
+        if [ -n "$var" ]; then
+            echo $var;
+            exit 0;
+        fi
+    done
+elif [ $ip_version == "6" ]; then
+    for interface in $interfaces;
+    do
+        var=$(ip address show $interface 2>/dev/null | grep global | grep -v temporary | grep -o -P '(?<=inet6 ).*(?=/)')
+        if [ -n "$var" ]; then
+            echo $var;
+            exit 0;
+        fi
+    done
+else
+    >&2 echo "ERROR, Invalid IP version!"
+    exit 2
+fi
